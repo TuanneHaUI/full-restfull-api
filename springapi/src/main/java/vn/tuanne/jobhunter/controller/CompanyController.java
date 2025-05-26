d15d0635c315bf75485f011cca68c4b9e3a130c0
TuanneHaUI <tuanne2542004@gmail.com>
2025-05-26 10:25:16 +0700

    first commit

---
+++ springapi/src/main/java/vn/tuanne/jobhunter/controller/CompanyController.java
@@ -0,0 +1,73 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.turkraft.springfilter.boot.Filter;
+
+import jakarta.validation.Valid;
+import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.domain.response.ResultPaginationDTO;
+import vn.hoidanit.jobhunter.service.CompanyService;
+import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @PostMapping("/companies")
+    public ResponseEntity<Company> createCompany(@Valid @RequestBody Company postmanCompany) {
+        Company company = this.companyService.handleCreateCompany(postmanCompany);
+        return ResponseEntity.status(HttpStatus.CREATED).body(company);
+    }
+
+    @GetMapping("/companies")
+    @ApiMessage("Fetch companys")
+    public ResponseEntity<ResultPaginationDTO> getCompany(
+            @Filter Specification<Company> spec, Pageable pageable) {
+
+        return ResponseEntity.ok(this.companyService.handleGetCompany(spec, pageable));
+    }
+
+    @PutMapping("/companies")
+    public ResponseEntity<Company> updateUser(@Valid @RequestBody Company reqCompany) {
+        Company company = this.companyService.handleUpdateCompany(reqCompany);
+        return ResponseEntity.ok(company);
+    }
+
+    @DeleteMapping("/companies/{id}")
+    public ResponseEntity<Void> deleteCompany(@PathVariable("id") long id) {
+        this.companyService.handleDeleteCompany(id);
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/companies/{id}")
+    @ApiMessage("Fetch company by id")
+    public ResponseEntity<Company> fetchCompanyById(@PathVariable("id") long id) {
+        Optional<Company> company = this.companyService.findById(id);
+        return ResponseEntity.ok().body(company.get());
+    }
+}
---
+++ springapi/src/main/java/vn/tuanne/jobhunter/service/CompanyService.java
@@ -0,0 +1,81 @@
+package vn.hoidanit.jobhunter.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.domain.response.ResultPaginationDTO;
+import vn.hoidanit.jobhunter.repository.CompanyRepository;
+import vn.hoidanit.jobhunter.repository.UserRepository;
+
+@Service
+public class CompanyService {
+
+    private final CompanyRepository companyRepository;
+    private final UserRepository userRepository;
+
+    public CompanyService(CompanyRepository companyRepository, UserRepository userRepository) {
+        this.companyRepository = companyRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Company handleCreateCompany(Company company) {
+        return this.companyRepository.save(company);
+    }
+
+    public ResultPaginationDTO handleGetCompany(Specification<Company> spec, Pageable pageable) {
+        Page<Company> pageCompany = this.companyRepository.findAll(spec, pageable);
+        ResultPaginationDTO rs = new ResultPaginationDTO();
+        ResultPaginationDTO.Meta mt = new ResultPaginationDTO.Meta();
+        mt.setPage(pageable.getPageNumber() + 1);
+        mt.setPageSize(pageable.getPageSize());
+        mt.setPages(pageCompany.getTotalPages());
+        mt.setTotal(pageCompany.getTotalElements());
+        rs.setMeta(mt);
+        rs.setResult(pageCompany.getContent());
+        return rs;
+    }
+
+    public Company fetchCompanyById(long id) {
+        Optional<Company> companyOptional = this.companyRepository.findById(id);
+        if (companyOptional.isPresent()) {
+            return companyOptional.get();
+        }
+        return null;
+    }
+
+    public Company handleUpdateCompany(Company company) {
+        Company currentCompany = this.fetchCompanyById(company.getId());
+        if (currentCompany != null) {
+            currentCompany.setAddress(company.getAddress());
+            currentCompany.setDescription(company.getDescription());
+            currentCompany.setName(company.getName());
+            currentCompany.setLogo(company.getLogo());
+            // update
+            currentCompany = this.companyRepository.save(currentCompany);
+        }
+        return currentCompany;
+    }
+
+    public void handleDeleteCompany(long id) {
+        Optional<Company> companyOptional = this.companyRepository.findById(id);
+        if (companyOptional.isPresent()) {
+            Company com = companyOptional.get();
+            // fetch all user belong to this company
+            List<User> users = this.userRepository.findByCompany(com);
+            this.userRepository.deleteAll(users);
+        }
+        this.companyRepository.deleteById(id);
+    }
+
+    public Optional<Company> findById(long id) {
+        return this.companyRepository.findById(id);
+    }
+
+}
